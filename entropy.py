6dc72614566b970df05a63d8bb73fb4f60c4dde6
sk413025 <sk413025@gmail.com>
2023-07-20 07:08:43 +0000

    ent_exp setting change

--- entropy.py
+++ entropy.py
@@ -4,8 +4,10 @@ from PIL import Image
 from math import ceil
 from scipy.stats import entropy
 import time
+import seaborn as sns
 
 img_path = 'adversarial_samples/'
+ws_ratio = 1
 
 def load_imgs(path):
     
@@ -18,7 +20,7 @@ def load_imgs(path):
     
     return imgs
 
-def get_heapmap(img, ws_ratio):
+def get_heatmap(img, ws_ratio):
     (size_x, size_y) = img.shape
     size_max = max(size_x, size_y)
     size_max = max(ceil(size_max/100), 8)
@@ -30,18 +32,18 @@ def get_heapmap(img, ws_ratio):
 
     heatmap = np.zeros((size_x + w_size, size_y + w_size))
     pad_img = np.pad(img, (int(w_size/2),), "symmetric")
-    
+
     # start = time.time()
     # a = np.unique(pad_img[0:w_size, 0:w_size].flatten(), return_counts=True)[1]
     # a = np.histogram(pad_img[0:w_size, 0:w_size].flatten(), range(256))[0]
 
     # a = entropy(a/sum(a))
-    
+
     # end = time.time()
     # print(f"time: {end - start:.5f} {a}")
-    
+
     ents = []
-    last = []
+    bound = []
     for x in range(0, pad_img.shape[0] - w_size, strd):
         for y in range(0, pad_img.shape[1] - w_size, strd):
             counts = np.unique(pad_img[x:x+w_size, y:y+w_size].flatten(), return_counts=True)[1]
@@ -51,21 +53,29 @@ def get_heapmap(img, ws_ratio):
             bound = [x+w_size, y+w_size]
     x_exc = bound[0] - size_x
     y_exc = bound[1] - size_y
-    print(f"ori_size: {size_x} X {size_y}\nent_szie: {bound[0]} X {bound[1]}")
-    print(x_exc>>1)
-    print("Before slicing:\nshape: ", heatmap.shape, "\n", heatmap)
+    # print(f"ori_size: {size_x} X {size_y}\nent_szie: {bound[0]} X {bound[1]}")
+    # print(x_exc>>1)
+    # print("Before slicing:\nshape: ", heatmap.shape, "\n", heatmap)
     heatmap = heatmap[(x_exc>>1)+(x_exc&1):bound[0]-(x_exc>>1), (y_exc>>1)+(y_exc&1):bound[1]-(y_exc>>1)]
-    print("After slicing:\nshpae: ", heatmap.shape, "\n", heatmap)
+    # print("After slicing:\nshpae: ", heatmap.shape, "\n", heatmap)
+
+    return heatmap
 
 
 if __name__ == '__main__':
-    # imgs = load_imgs(img_path)
-    # get_heapmap(imgs[0], 1)
-    # t = np.random.randint(0,8,(5,5))
-    # print(t[2:4, 2:4], '\n')
-    # t = t[2:-1, 2:-1]
-    # print(t, t.shape)
+    imgs = load_imgs(img_path)
 
-    test = np.random.randint(0, 256, (217, 212))
-    get_heapmap(test, 1)
+    # test = np.random.randint(0, 256, (217, 212))
+    heatmaps = []
+    for img in imgs:
+        heatmaps.append(get_heatmap(img, ws_ratio))
+
+    mean_ent = []
+    for i, heatmap in enumerate(heatmaps):
+        # print(heatmap)
+        mean = np.mean(heatmap)
+        print(f"{i:2}: mean entropy {mean}")
+
+        hstg = sns.histplot(heatmap.flatten(), kde=False, color='blue')
+        hstg.get_figure().savefig(f"exp/hstg{i}.png")
     # print(imgs[0].shape)
